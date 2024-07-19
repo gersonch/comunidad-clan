96041c14b5d683480a8aa7fb7034aa6df6a22319
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-07-19 17:27:26 -0400

    Primer commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      "bg-img": "url(/public/bg-img.jpg)",
+    },
+  },
+  plugins: [],
+};
