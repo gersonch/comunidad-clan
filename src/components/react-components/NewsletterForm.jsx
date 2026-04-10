183e356bed5e0d9d5e3885ac276a92f701178cf1
gersonch <78560393+gersonch@users.noreply.github.com>
2026-04-10 11:32:20 -0400

    url fix

--- src/components/react-components/NewsletterForm.jsx
+++ src/components/react-components/NewsletterForm.jsx
@@ -17,7 +17,7 @@ function NewsletterForm() {
     try {
       setLoading(true);
       const response = await fetch(
-        apiUrl,
+        `${apiUrl}/create`,
         {
           method: "POST",
           headers: {
